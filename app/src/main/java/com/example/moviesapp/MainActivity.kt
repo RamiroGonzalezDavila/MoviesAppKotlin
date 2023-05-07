6754d99479f4f93b1b4a2f94ee4fce4bba41cc5d
RamiroGonzalezDavila <ram_yosh222_@hotmail.com>
2023-05-07 00:24:54 -0600

    creacion del fragment 1 con estructura xml para el fragment y main

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.moviesapp
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,23 +11,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moviesapp.models.Movie
 import com.example.moviesapp.models.MoviesResponse
 import com.example.moviesapp.services.MovieApiService
 import com.example.moviesapp.services.MoviesApiInterface
 import com.example.moviesapp.ui.theme.MoviesAppTheme
-import kotlinx.android.synthetic.main.activity_main.np_movies_list
-import kotlinx.android.synthetic.main.activity_main.rv_movies_list
+import kotlinx.android.synthetic.main.fragment_fragmento1.np_movies_list
+import kotlinx.android.synthetic.main.fragment_fragmento1.rv_movies_list
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import androidx.fragment.app.commit
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+            fragmentTrans.replace<Fragmento1>(R.id.frameContainer)
+            fragmentTrans.commit()
+
+
+
 
         rv_movies_list.layoutManager =LinearLayoutManager(this)
         rv_movies_list.setHasFixedSize(true)
